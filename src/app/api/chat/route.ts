c1b65a50c714d25a71d186044b55c34ab7dcfb72
Daniel Navarro <danielkristopher0563@gmail.com>
2024-07-10 09:47:32 -0500

    +geminis

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,4 +1,5 @@
-import { OpenAIStream, StreamingTextResponse } from "ai";
+//uso OpenAI
+/*import { OpenAIStream, StreamingTextResponse } from "ai";
 import { Configuration, OpenAIApi } from "openai-edge";
 
 // Create an OpenAI API client (that's edge friendly!)
@@ -24,4 +25,46 @@ export async function POST(req: Request) {
   const stream = OpenAIStream(response);
   // Respond with the stream
   return new StreamingTextResponse(stream);
+}
+*/
+
+//Uso Gemini
+import { StreamingTextResponse, GoogleGenerativeAIStream, Message } from "ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  const reqBody = await req.json();
+  const messages: Message[] = reqBody.messages;
+
+  // Define el nombre del modelo a usar
+  const modelName = "gemini-1.5-flash";
+
+  // Construye el prompt para la API de Google Generative AI
+  const promptWithParts = buildGoogleGenAIPrompt(messages);
+
+  // Crea el cliente de Google Generative AI
+  const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
+  const model = genAI.getGenerativeModel({ model: modelName });
+
+  // Genera la respuesta de forma continua
+  const streamingResponse = await model.generateContentStream(promptWithParts);
+
+  // Devuelve la respuesta en un formato de streaming
+  return new StreamingTextResponse(GoogleGenerativeAIStream(streamingResponse));
+}
+
+function buildGoogleGenAIPrompt(messages: Message[]) {
+  return {
+    contents: messages
+      .filter(
+        (message) => message.role === "user" || message.role === "assistant"
+      )
+      .map((message) => ({
+        role: message.role === "user" ? "user" : "model",
+        parts: [{ text: message.content }],
+      })),
+  };
 }
